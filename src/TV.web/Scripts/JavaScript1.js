c5ff5a20d041d74413423c0535df5b6c4bc1bb6c
paradigm944 <joadgilbert@hotmail.com>
2013-12-02 21:08:10 -0600

    added id to view links, auth if statement on index

--- src/TV.web/Scripts/JavaScript1.js
+++ src/TV.web/Scripts/JavaScript1.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
                             "bSearchable": false,
                             "bSortable": false,
                             "fnRender": function (oObj) {
-                                return '<a href=\"Search/ViewPost/' + oObj.aData[0] + '\">View</a>';
+                                return '<a id="viewLink" href=\"Search/ViewPost/' + oObj.aData[0] + '\">View</a>';
                             }
                         },
 			            { "sName": "Landlord" },
