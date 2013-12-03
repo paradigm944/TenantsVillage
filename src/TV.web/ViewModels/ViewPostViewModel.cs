4bec1670702013afec351867a2329a24cae2cfc9
paradigm944 <joadgilbert@hotmail.com>
2013-12-02 18:14:14 -0600

    added login properties

--- src/TV.web/ViewModels/ViewPostViewModel.cs
+++ src/TV.web/ViewModels/ViewPostViewModel.cs
@@ -13,6 +13,18 @@ namespace TV.web.ViewModels
 {
     public class ViewPostViewModel 
     {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+
         public List<string> Pics { get; set; }
 
         [DisplayName("Post")]
