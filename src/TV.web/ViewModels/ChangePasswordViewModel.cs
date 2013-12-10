6f00dc6b6577a265ed559279514f4b602b6557d4
paradigm944 <joadgilbert@hotmail.com>
2013-12-09 18:36:38 -0600

    swapped names

---
+++ src/TV.web/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+using System.ComponentModel.DataAnnotations;
+using TV.web.Models;
+
+
+namespace TV.web.ViewModels
+{
+    public class ChangePasswordViewModel 
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        
+        [Display(Name = "Confirm password")]
+        [System.ComponentModel.DataAnnotations.CompareAttribute("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
