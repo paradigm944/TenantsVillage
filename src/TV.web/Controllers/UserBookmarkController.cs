87f4f8d0cfd395d3c1380a7387700ccec5385155
paradigm944 <joadgilbert@hotmail.com>
2014-01-05 11:50:36 -0600

    added bkmrk controller

---
+++ src/TV.web/Controllers/UserBookmarkController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TV.web.Models;
+
+namespace TV.web.Controllers
+{
+    public class UserBookmarkController : Controller
+    {
+        private readonly TVContext _ctx;
+       
+        public UserBookmarkController(TVContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpPost]
+        public JsonResult Bookmark()
+        {
+            var message = "Bookmark Success";
+            return Json(message, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
