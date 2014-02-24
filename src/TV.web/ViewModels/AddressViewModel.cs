0b1fe962e17542cf1962421190501bdf1923f750
paradigm944 <joadgilbert@hotmail.com>
2014-02-24 10:58:27 -0600

    add addressVM and marker pin icon

---
+++ src/TV.web/ViewModels/AddressViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TV.web.ViewModels
+{
+    public class AddressViewModel
+    {
+        public string ZipCode { get; set; }
+
+        public bool? IsZipSearch { get; set; }
+    }
+}
